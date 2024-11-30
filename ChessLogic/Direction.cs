ec4d078369158c8387e50159576619242a7297d8
cloudflips32 <devadamstandish@gmail.com>
2024-11-30 00:08:20 -0500

    Combine Directions, Scale Directions, Direction

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,24 @@
+﻿namespace ChessLogic
+{
+    public class Direction
+    {
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction dir1, Direction dir2)
+        { 
+            return new Direction(dir1.RowDelta+dir2.RowDelta, dir1.ColumnDelta+dir2.ColumnDelta);
+        }
+
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+    }
+}
