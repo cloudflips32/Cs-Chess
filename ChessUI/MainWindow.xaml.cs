2b120bcb5fa9dd0caab981894f957f3d70686f50
cloudflips32 <devadamstandish@gmail.com>
2025-01-06 17:20:46 -0500

    board drawn, pieces show up in initial places, removed unused usings

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ChessLogic;
 
 namespace ChessUI
@@ -18,10 +10,15 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();
             InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.initial());
+            DrawBoard(gameState.Board);
         }
 
         private void InitializeBoard()
@@ -33,6 +30,18 @@ namespace ChessUI
                     Image image = new Image();
                     pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for( int r = 0;r < 8; r++)
+            {
+                for( int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r,c];
+                    pieceImages[r,c].Source = Images.GetImage(piece);
                 }
             }
         }
