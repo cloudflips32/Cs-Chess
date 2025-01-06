30cdc8c92a75d8f4e65a2494b2829b1d19c7dbe9
cloudflips32 <devadamstandish@gmail.com>
2025-01-06 15:04:32 -0500

    piece characteristics defined

--- ChessLogic/OurPieces/Bishop.cs
+++ ChessLogic/OurPieces/Bishop.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic.OurPieces
+﻿namespace ChessLogic
 {
-    internal class Bishop
+    public class Bishop : Piece
     {
+        public override PieceType Type => PieceType.Bishop;
+        public override Player Color { get; }
+        public Bishop(Player color) 
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Bishop copy = new Bishop(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
     }
 }
--- ChessLogic/OurPieces/Knight.cs
+++ ChessLogic/OurPieces/Knight.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic.OurPieces
+﻿namespace ChessLogic
 {
-    internal class Knight
+    public class Knight : Piece
     {
+        public override PieceType Type => PieceType.Knight;
+        public override Player Color { get; }
+
+        public Knight(Player color)
+        {
+            Color = color;
+        }
+        public override Piece Copy()
+        {
+            Knight copy = new Knight(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
     }
 }
