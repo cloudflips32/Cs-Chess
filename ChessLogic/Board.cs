4ddd31cafc79e539bad2609758bc944f1d5f607a
cloudflips32 <devadamstandish@gmail.com>
2025-01-06 15:27:19 -0500

    pieces are set at the start of the game

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -15,5 +15,43 @@
             get { return this[pos.Row, pos.Column]; }
             set { this[pos.Row, pos.Column] = value; }
         }
+
+        public static Board initial()
+        {
+            Board board = new Board();
+
+            board.AddStartPieces();
+
+            return board;
+        }
+
+        private void AddStartPieces()
+        {
+            this[0,0] = new Rook(Player.Black);
+            this[0,1] = new Knight(Player.Black);
+            this[0,2] = new Bishop(Player.Black);
+            this[0,3] = new Queen(Player.Black);
+            this[0,4] = new King(Player.Black);
+            this[0,5] = new Bishop(Player.Black);
+            this[0,6] = new Knight(Player.Black);
+            this[0,7] = new Rook(Player.Black);
+
+            this[7,0] = new Rook(Player.White);
+            this[7,1] = new Knight(Player.White);
+            this[7,2] = new Bishop(Player.White);
+            this[7,3] = new Queen(Player.White);
+            this[7,4] = new King(Player.White);
+            this[7,5] = new Bishop(Player.White);
+            this[7,6] = new Knight(Player.White);
+            this[7,7] = new Rook(Player.White);
+
+            int p = 0;
+            while(p<8)
+            {
+                this[1,p] = new Pawn(Player.Black);
+                this[6,p] = new Pawn(Player.White);
+                p++;
+            }
+        }
     }
 }
